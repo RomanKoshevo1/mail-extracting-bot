2b556296a644bc480e073515d472e4298f932328
Roman <koshevoi.home@gmail.com>
2023-12-21 15:56:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+require('dotenv').config();
+const { Bot, Keyboard, InlineKeyboard, GrammyError, HttpError } = require('grammy');
+const bot = new Bot(process.env.BOT_API_KEY);
+const { getRandomQuestion, getCorrectAnswer } = require('./utils');
+
+bot.command('start', async (ctx) => {
+    const startKeyboard = new Keyboard()
+        .text('HTML')
+        .text('CSS')
+        .row()
+        .text('JavaScript')
+        .text('React')
+        .row()
+        .text('Random Question')
+        .resized();
+    await ctx.reply(`Hello, ${ctx.update.message.from.first_name}! \nI am your exam assistante!`);
+    await ctx.reply('Please choice the topic:', {
+        reply_markup: startKeyboard
+    })
+})
+
+bot.hears(['HTML', 'CSS', 'JavaScript', 'React', 'Random Question'], async (ctx) => {
+    
+    const topic = ctx.message.text.toLowerCase();
+    const {question, questionTopic} = getRandomQuestion(topic);
+
+    let inlineKeyboard;
+    
+    
+    if (question.hasOptions) {
+        const buttonRows = question.options.map((option) =>  [
+            InlineKeyboard.text(
+                option.text , 
+                JSON.stringify({
+                    type: `${questionTopic}-option`,
+                    isCorrect: option.isCorrect, 
+                    questionId: question.id,
+                })
+            )
+        ]);
+        
+        inlineKeyboard = InlineKeyboard.from(buttonRows);
+        
+    } else {
+        inlineKeyboard = new InlineKeyboard()
+        .text('Get answer', JSON.stringify({
+            type: questionTopic,
+            questionId: question.id,
+        }))
+    }
+
+
+        await ctx.reply(question.text, {
+        reply_markup: inlineKeyboard
+    });
+})
+
+bot.on('callback_query:data', async (ctx) => {
+    const callbackData = JSON.parse(ctx.callbackQuery.data);
+
+    if(!callbackData.type.includes('option')){
+        const answer = getCorrectAnswer(callbackData.type, callbackData.questionId);
+        await ctx.reply(answer, {
+            parse_mode: 'HTML',
+            disable_web_page_preview: true,
+        });
+        await ctx.answerCallbackQuery();
+        return;
+    }
+
+    if(callbackData.isCorrect){
+        await ctx.reply('It correct');
+        await ctx.answerCallbackQuery();
+        return;
+    };
+
+    const answer = getCorrectAnswer(callbackData.type.split('-')[0], callbackData.questionId);
+    await ctx.reply(`Incorret. Correct answer - ${answer}`);
+    await ctx.answerCallbackQuery();
+    
+})
+
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+      console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+      console.error("Could not contact Telegram:", e);
+    } else {
+      console.error("Unknown error:", e);
+    }
+  });
+
+bot.start();
